3859078e2bbaef6c12045b1446bea47245225002
pained-chicken <endusj01@kakao.com>
2026-07-08 20:20:08 +0900

    tools 불러오기 변경

--- student_parts/week02_structure_natural_language_requests.py
+++ student_parts/week02_structure_natural_language_requests.py
@@ -149,7 +149,7 @@ def week02_tools() -> list[Any]:
     """Week 2 agent에 Week 1 도구를 노출해 tool JSON을 structured_response 근거로 씁니다."""
 
     # TODO: Week 1에서 구현한 tool 목록을 그대로 반환하세요.
-    return [personal_create_schedule, personal_list_schedules, personal_delete_schedule]
+    return week01_tools
     ...
 
 
@@ -157,6 +157,7 @@ def week02_system_prompt() -> str:
     """2주차 agent가 따르는 시스템 프롬프트입니다."""
 
     # TODO: join_system_prompt(...)로 week02_prompt_parts()와 Week 2 structured_response 최종 답변 규칙을 합치세요.
+    join_system_prompt()
     # TODO: StructuredRequestBatch에는 요청이 하나뿐이어도 requests 목록에 StructuredRequest 하나를 담도록 지시하세요.
     # TODO: personal_create_schedule tool 결과 JSON의 created_schedule을 읽어 필드를 채우도록 지시하세요.
     ...
