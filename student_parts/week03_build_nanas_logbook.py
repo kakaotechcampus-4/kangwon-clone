4070a1388a7ea7162840ce701d6d12d7c8c86fcf
gkrdls <gkrdls556@gmail.com>
2026-07-15 16:32:58 +0900

    feat: personal_update_saved_schedule 구현

--- student_parts/week03_build_nanas_logbook.py
+++ student_parts/week03_build_nanas_logbook.py
@@ -547,6 +547,24 @@ def personal_update_saved_schedule(
 
     # TODO: None이 아닌 수정 필드를 AppSQLiteStore.update_schedule(...)에 전달하세요.
     # TODO: ID가 없으면 ok=False, 있으면 updated_schedule/shared_sync를 담아 JSON 문자열로 반환하세요.
+    result = _store().update_schedule(schedule_id, title, date, start_time, end_time, attendees)
+
+    if result is None:
+        return json_payload(
+            tool_result(
+                ok=False,
+                tool_name="personal_update_saved_schedule"
+            )
+        )
+    
+    return json_payload(
+        tool_result(
+            ok=True,
+            tool_name="personal_update_saved_schedule",
+            updated_schedule=result.get("schedule"),
+            shared_sync=result.get("shared_sync")
+        )
+    ) 
 
 
 @tool(args_schema=SavedScheduleDeleteInput)
