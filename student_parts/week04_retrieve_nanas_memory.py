8d12ac3351eeb04e8c351075391375647c971cad
SumiNK03 <0417nam@naver.com>
2026-07-22 15:27:47 +0900

    week4 : search_saved_requests 구현

--- student_parts/week04_retrieve_nanas_memory.py
+++ student_parts/week04_retrieve_nanas_memory.py
@@ -328,8 +328,9 @@ def search_personal_references(query: str, top_k: int = 2) -> str:
 def search_saved_requests(query: str, top_k: int = 3) -> str:
     """SQLite에 저장된 구조화 일정/할 일/알림 row를 검색합니다. query에는 LLM이 고른 일정/할 일/알림 핵심어를 넣습니다."""
 
-    # TODO: AppSQLiteStore.search_saved_requests(...)로 저장 요청을 검색하고 top-level rows를 반환하세요.
-    ...
+    # 저장된 요청을 검색
+    rows = search_saved_request_rows(SQLITE_STORE, query=query, top_k=top_k)
+    return json_payload({"rows": rows})
 
 
 @tool(args_schema=SearchConversationMessagesInput)
