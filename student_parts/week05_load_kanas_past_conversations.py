3e128ae6916d440a1c38181c41a3c5d5a0695230
pained-chicken <endusj01@kakao.com>
2026-07-29 01:31:37 +0900

    list_shared_schedules 구현

--- student_parts/week05_load_kanas_past_conversations.py
+++ student_parts/week05_load_kanas_past_conversations.py
@@ -468,8 +468,15 @@ def list_shared_schedules(
 ) -> str:
     """외부 MCP 공유 일정 저장소에 등록된 일정을 조회합니다. 필터가 없으면 기본 공유 일정을 반환합니다."""
 
-    # TODO: call_mcp_tool_sync("list_shared_schedules", args)로 공유 일정 저장소 rows를 조회하세요.
-    ...
+    picked_schedules = call_mcp_tool_sync("list_shared_schedules", {
+        "member_names": member_names,
+        "date_from": date_from,
+        "date_to": date_to,
+        "source_conversation_id": source_conversation_id,
+        "limit": limit
+    })
+    
+    return picked_schedules
 
 
 # [메인] collect_member_schedules(...)
