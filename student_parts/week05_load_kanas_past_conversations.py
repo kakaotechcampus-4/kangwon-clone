90cac102c2c90f67408f04479b9d77ed37f313f0
Roka-jsj <vasebull@gmail.com>
2026-07-30 14:30:07 +0900

    delete shared schedule

--- student_parts/week05_load_kanas_past_conversations.py
+++ student_parts/week05_load_kanas_past_conversations.py
@@ -384,8 +384,11 @@ def delete_shared_schedule(
 ) -> str:
     """외부 MCP 공유 일정 저장소에서 일정을 삭제합니다."""
 
-    # TODO: call_mcp_tool_sync("delete_shared_schedule", args)로 공유 일정을 삭제하세요.
-    ...
+    delete_schedule = call_mcp_tool_sync("delete_shared_schedule", {
+        "schedule_id": schedule_id,
+        "source_conversation_id": source_conversation_id,
+    })
+    return delete_schedule
 
 
 @tool(args_schema=ListSharedSchedulesInput)
