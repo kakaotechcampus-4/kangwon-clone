63da4a542cf742e762659ae40fbb206ac7a8c45c
pained-chicken <endusj01@kakao.com>
2026-08-01 16:15:13 +0900

    '?' 스트링 제거, 빈 스트링으로  변경

--- student_parts/week05_load_kanas_past_conversations.py
+++ student_parts/week05_load_kanas_past_conversations.py
@@ -362,7 +362,7 @@ def _collect_member_schedules(
                 "date": request.date,
                 "start_time": request.start_time,
                 "end_time": request.end_time,
-                "notes": "?",
+                "notes": "",
             }
         )
 
